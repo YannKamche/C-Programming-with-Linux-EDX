3fd284a5f8d7ac0f0272d90d73cfad43328bbb77
Yann Kamche <yannkamche@gmail.com>
2023-04-25 16:20:25 +0100

    Repeat one statement with a for loop

---
+++ Getting Started/Repeat one statement with a for loop/repeatWithLoop.c
@@ -0,0 +1,15 @@
+/*
+C is fun!
+C is fun!
+C is fun!
+*/
+#include <stdio.h>
+int main(void) {
+    
+    int i;
+    for(i = 0; i < 3; i++) 
+    {
+        printf("C is fun!\n");
+    }
+    return 0;
+}
