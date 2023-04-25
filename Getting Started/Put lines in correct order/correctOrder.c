8c120fffd0ac3fe52cd8e492056692b195d15eed
Yann Kamche <yannkamche@gmail.com>
2023-04-25 21:00:18 +0100

    Correct order

---
+++ Getting Started/Put lines in correct order/correctOrder.c
@@ -0,0 +1,25 @@
+//preprocessor directive
+#include <stdio.h>
+//main function
+int main(void){
+    //variable declaration
+     int i;
+     //executable statement
+printf("I ");
+
+    for(i = 0; i < 3 ; i++) {
+        printf("love ");
+    }
+ printf("programming!");
+ //return statement
+    return(0);
+
+   
+
+    
+
+}
+
+
+
+   
