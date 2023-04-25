0dccae68cc51e4958daa80fe57a1e96fb35699a1
Yann Kamche <yannkamche@gmail.com>
2023-04-25 16:14:34 +0100

    Quotation marks and escape special characters

---
+++ Getting Started/Quotation marks and escape special characters/quotation.c
@@ -0,0 +1,10 @@
+/*
+Dennis Ritchie said:                                                            
+"The only way to learn a new programming language is by writing programs in it."
+*/
+#include <stdio.h>
+int main() {
+    printf("Dennis Ritchie said:\n\"The only way to learn a new programming language is by writing programs in it.\"");
+    return 0;
+    
+}
