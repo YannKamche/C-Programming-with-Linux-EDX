a20124cf674a3cb83ece855638914d483e935429
Yann Kamche <yannkamche@gmail.com>
2023-04-25 16:54:42 +0100

    printing multiple block of lines with a for loop

---
+++ Getting Started/Printing multiple block of lines with a for loop/printLines.c
@@ -0,0 +1,29 @@
+/*
+C is fun!
+C is fun!
+C is fun!
+
+We can do everything with it!
+We can do everything with it!
+We can do everything with it!
+We can do everything with it!
+We can do everything with it!
+*/
+
+#include <stdio.h>
+int main()
+ {
+     int i;
+     for (i = 0; i < 3; i++)
+     {
+         printf("C is fun!\n");
+     }
+       
+    printf("\n");
+    for (i = 0; i < 5; i++)
+    {
+         printf("We can do everything with it!\n");
+    }
+    
+     return 0;
+ }
