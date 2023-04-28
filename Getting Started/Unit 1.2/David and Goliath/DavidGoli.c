840b0cbe45d452967bac40b01888522c6c1ad472
Yann Kamche <yannkamche@gmail.com>
2023-04-28 20:42:38 +0100

    moidified

--- Getting Started/Unit 1.2/David and Goliath/DavidGoli.c
+++ Getting Started/Unit 1.2/David and Goliath/DavidGoli.c
@@ -4,14 +4,17 @@ int main()
  {
     int botNum, i, power;
     power = 0;
+    printf("Number of robots: ");
     scanf("%d", &botNum);
     
     for(i = 0; i < botNum; i++)
     {
         int height, weight, enginePower, resistance;
+        printf("Enter the height, weight, power and resistance of robot %d: ", ++i);
+        
         scanf("%d %d %d %d", &height, &weight, &enginePower, &resistance);
         power += ((enginePower + resistance) * (weight - height));
     }
-    printf("%d", power);
+    printf("The power is : %d", power);
     return 0;
  }
